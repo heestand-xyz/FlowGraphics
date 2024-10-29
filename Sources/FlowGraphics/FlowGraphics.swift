aa91c4c5f8e2dc51e4f6e32141dd81a4c2f70b8c
Anton Heestand <anton.heestand@gmail.com>
2024-10-29 13:19:43 +0900

    First Color Test

--- Sources/FlowGraphics/FlowGraphics.swift
+++ Sources/FlowGraphics/FlowGraphics.swift
@@ -12,16 +12,18 @@ import CoreFlowGraphics
 
 extension Graphic {
     
-    public func flow() {
+    /// Top left color of the graphic.
+    /// The graphics must be 8 bit (default).
+    public func topLeftPixelColor() -> PixelColor {
+        precondition(bits == ._8)
         let texturePointer = UnsafeMutableRawPointer(Unmanaged.passUnretained(texture).toOpaque())
         var cfgs = CoreFlowGraphics()
         let color = cfgs.flow(texturePointer)
-        let pixelColor = PixelColor(
+        return PixelColor(
             red255: Int(color.red),
             green255: Int(color.green),
             blue255: Int(color.blue),
             alpha255: Int(color.alpha)
         )
-        print("Pixel Color:", pixelColor)
     }
 }
--- Tests/FlowGraphicsTests/FlowGraphicsTests.swift
+++ Tests/FlowGraphicsTests/FlowGraphicsTests.swift
@@ -2,8 +2,9 @@ import Testing
 @testable import FlowGraphics
 import CoreGraphics
 import AsyncGraphics
+import PixelColor
 
 @Test func example() async throws {
     let graphic: Graphic = try await .color(.orange, resolution: CGSize(width: 1, height: 1))
-    graphic.flow()
+    try await #expect(graphic.topLeftPixelColor() == PixelColor.orange)
 }
